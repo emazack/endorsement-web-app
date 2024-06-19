e9c7957acdda93dfdefacdf736393411aec823d0
emazack <em.zaccaria@gmail.com>
2024-06-19 16:05:49 +0200

    Write pseudo code

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-// accedere al database
+// Importare l'inizializzatore di database
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js"
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js"
+// importare la funzione per recupeare il database
+// importare eventualmente altre funzioni e metodo che servono per gestire il database
+
+// Creare una costante con le proprietà di firebase per esempio databaseURL
+
+// Inizializzare il database
+// Recuperare il database
+
 const database = "https://realtime-database-9afdc-default-rtdb.europe-west1.firebasedatabase.app/"
