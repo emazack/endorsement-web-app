b595484df030464889641a829e8b96c75a2b5459
emazack <em.zaccaria@gmail.com>
2024-06-21 11:11:52 +0200

    Create a reference

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js"
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js"
+import { getDatabase, ref, get } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js"
 
 //First initialize the fire base config. Write down the data that you have
 const firebaseConfig = {
-    // apiKey: "YOUR_API_KEY",
-    // authDomain: "YOUR_AUTH_DOMAIN",
     databaseURL: "https://realtime-database-9afdc-default-rtdb.europe-west1.firebasedatabase.app/",
-    // projectId: "YOUR_PROJECT_ID",
-    // storageBucket: "YOUR_STORAGE_BUCKET",
-    // messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    // appId: "YOUR_APP_ID"
 };
 
 //Second inizialize the Firebase app. Connect the data that you have with firebase
 const app = initializeApp(firebaseConfig)
+
 //Finally get the database from your firebase app
 const database = getDatabase(app)
+
+//Create a reference, a path. Connect the database with the reference
+const endorsementDbRef = ref(database, "/endorsement")
+
+// console.log();
 
 
