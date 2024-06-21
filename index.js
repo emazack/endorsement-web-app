7dcf4c266da1f0fb094807d1639ee5e021a52566
emazack <em.zaccaria@gmail.com>
2024-06-21 10:18:07 +0200

    Init the js and add the type module

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-// Importare l'inizializzatore di database
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js"
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js"
-// importare la funzione per recupeare il database
-// importare eventualmente altre funzioni e metodo che servono per gestire il database
-
-// Creare una costante con le proprietà di firebase per esempio databaseURL
-
 // Inizializzare il database
 // Recuperare il database
 
-const database = "https://realtime-database-9afdc-default-rtdb.europe-west1.firebasedatabase.app/"
+const configDatabase = {
+    databaseUrl: "https://realtime-database-9afdc-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+
+const database = getDatabase(configDatabase.databaseUrl)
+const app = initializeApp(database)
+
+console.log(app);
+
