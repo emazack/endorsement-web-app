f68082cc9dcd9a9afd09b1bfe6a86390bd6c51e0
emazack <em.zaccaria@gmail.com>
2024-06-21 10:20:01 +0200

    Refactor and fix the firebaseConfig

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js"
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js"
-// Inizializzare il database
-// Recuperare il database
 
-const configDatabase = {
-    databaseUrl: "https://realtime-database-9afdc-default-rtdb.europe-west1.firebasedatabase.app/"
-}
+const firebaseConfig = {
+    // apiKey: "YOUR_API_KEY",
+    // authDomain: "YOUR_AUTH_DOMAIN",
+    databaseURL: "https://realtime-database-9afdc-default-rtdb.europe-west1.firebasedatabase.app/",
+    // projectId: "YOUR_PROJECT_ID",
+    // storageBucket: "YOUR_STORAGE_BUCKET",
+    // messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    // appId: "YOUR_APP_ID"
+};
 
 
 const database = getDatabase(configDatabase.databaseUrl)
